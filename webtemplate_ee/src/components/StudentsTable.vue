2fd7f60bfc49d062e35ed035c33483a58ad5c5f2
TeAnore <chaplygin.a.o@gmail.com>
2022-01-10 07:50:21 +0400

    add webtemplate_ee

---
+++ webtemplate_ee/src/components/StudentsTable.vue
@@ -0,0 +1,146 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        <h1> Students table</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn @click="addNewRowStart">Add new student</v-btn>
+      </v-col>
+    </v-row>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th
+              class="text-center"
+              v-for="item in table_header"
+              v-bind:key="item"
+            >
+              {{ item }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in table_data" v-bind:key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.email }}</td>
+            <td>
+              <v-btn icon @click="editRowStart(item)">
+                <v-icon>mdi-pen</v-icon>
+              </v-btn>
+              <v-btn icon @click="deleteRow(item.id)">
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    
+    <dialog-example
+      :dialog="showDialog"
+      :item="newItem"
+      :title="title"
+      @close="closeDialog"
+      @next="nextFunction"
+    />
+  </div>
+</template>
+
+<script>
+// Change this import in respect to the technology currently using in the backend 
+import { eeGetAll, eeDeleteItem, eeAddNewItem, eeUpdateItem } from "../endpoints/table_students_endpoints";
+import DialogExample from "./DialogExample.vue";
+
+export default {
+  components: { DialogExample },
+  name: "Students Table",
+  data() {
+    return {
+      table_header: ["Id", "Name", "Email"],
+      table_data: [],
+      showDialog: false,
+      newItem: {name: '', email: ''},
+      editingItemIndex: -1,
+      title: '',
+    };
+  },
+  computed: {},
+  mounted() {
+    this.getAllRows();
+  },
+  methods: {
+    nextFunction() {
+      // Placeholder
+    },
+    getAllRows() {
+      eeGetAll().then((response) => {
+          this.table_data = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    deleteRow(itemId) {
+      eeDeleteItem(itemId)
+        .then(() => {
+          this.getAllRows();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    editRowStart(item) {
+      this.title = `Modify existing student #${item.id}`
+      this.editingItemIndex = item.id
+      this.newItem = {'name': item.name, 'email': item.email}
+      this.nextFunction = this.editRowEnd
+      this.showDialog = true;
+    },
+    editRowEnd(item) {
+      const body = {'name': item.name, 'email': item.email}
+      const params = {}
+      eeUpdateItem(this.editingItemIndex, params, body)
+        .then((response) => {
+          console.log(response.data);
+          this.getAllRows();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      this.editingItemIndex = -1
+      this.closeDialog()
+    },
+    addNewRowStart() {
+      this.title = 'Add new student'
+      this.newItem = {name: '', email: ''}
+      this.nextFunction = this.addNewRowEnd
+      this.showDialog = true;
+    },
+    addNewRowEnd(item) {
+      const body = {'name': item.name, 'email': item.email}
+      const params = {}
+      eeAddNewItem(params, body)
+        .then((response) => {
+          console.log(response.data);
+          this.getAllRows();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      this.closeDialog()
+    },
+    closeDialog() {
+      this.showDialog = false
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
